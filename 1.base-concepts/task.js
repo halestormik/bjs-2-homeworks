ba781a0d68782e5162801a94b5846c09314be210
NetoIQA75 <all-slavin@yandex.ru>
2024-04-09 14:05:36 +0300

    All tasks solved

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,8 @@
 "use strict"
 
 function solveEquation(a, b, c) {
-	let arr = [];
-	let discriminant = Math.pow(b, 2) - 4 * a * c;
+	const arr = [];
+	const discriminant = Math.pow(b, 2) - 4 * a * c;
 
 	if (discriminant === 0) {
 		arr.push(-b / (2 * a));
@@ -15,5 +15,18 @@ function solveEquation(a, b, c) {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+
+	percent = +percent;
+	contribution = +contribution;
+	amount = +amount;
+	countMonths = +countMonths;
+
+	percent = percent / 100;
+	const monthlyPercent = percent / 12; // месячная ставка
+	const creditBody = amount - contribution; // тело кредита = сумма кредита минус первоначальный взнос
+	const monthlyPay = creditBody * (monthlyPercent + (monthlyPercent / (Math.pow((1 + monthlyPercent), countMonths) - 1))); // ежемесячный платеж
+	const summaryPayment = (monthlyPay * countMonths).toFixed(2); // общая сумма выплат
+
+	return +summaryPayment;
+
 }
