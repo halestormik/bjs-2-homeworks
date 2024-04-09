c90769a08c9a7fb8fbb024b19860029363a3193f
NetoIQA75 <all-slavin@yandex.ru>
2024-04-09 12:55:57 +0300

    First task solution fixed

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -4,7 +4,7 @@ function solveEquation(a, b, c) {
 	let arr = [];
 	let discriminant = Math.pow(b, 2) - 4 * a * c;
 
-	if (discriminant = 0) {
+	if (discriminant === 0) {
 		arr.push(-b / (2 * a));
 	} else if (discriminant > 0) {
 		arr.push((-b + Math.sqrt(d)) / (2 * a));
