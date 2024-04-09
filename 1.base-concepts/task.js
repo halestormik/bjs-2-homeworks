d2e584af988486502feb22f2eb3a6dbc26a8de70
NetoIQA75 <all-slavin@yandex.ru>
2024-04-09 12:28:19 +0300

    First task solved

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,17 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr = [];
+	let discriminant = Math.pow(b, 2) - 4 * a * c;
+
+	if (discriminant = 0) {
+		arr.push(-b / (2 * a));
+	} else if (discriminant > 0) {
+		arr.push((-b + Math.sqrt(d)) / (2 * a));
+		arr.push((-b - Math.sqrt(d)) / (2 * a));
+	}
+
+	return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
