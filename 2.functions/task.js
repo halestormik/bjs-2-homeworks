d3e49d945c68cd2f45017d46f041449be6716ad6
NetoIQA75 <all-slavin@yandex.ru>
2024-04-15 10:40:52 +0300

    Task 2 completed

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,95 @@
 function getArrayParams(...arr) {
+	let min = arr[0];
+	let max = arr[0];
+	let sum = 0;
 
-  return { min: min, max: max, avg: avg };
+	// первый подход
+
+	/*for (let i = 0; i < arr.length; i++) { 
+	  if (arr[i] > max) {
+	    max = arr[i];
+	  } else if (arr[i] < min) {
+	    min = arr[i];
+	  }
+	  sum += arr[i];
+	}*/
+
+	// второй подход
+
+	min = Math.min.apply(null, arr);
+	max = Math.max.apply(null, arr);
+	const initialValue = 0;
+	sum = arr.reduce(
+		(accumulator, currentValue) => accumulator + currentValue,
+		initialValue,
+	);
+
+	let avg = Number((sum / arr.length).toFixed(2));
+	return {
+		min,
+		max,
+		avg
+	};
 }
 
 function summElementsWorker(...arr) {
-
+	if (arr.length === 0) {
+		return 0;
+	}
+	let sum = 0;
+	for (let i = 0; i < arr.length; i++) {
+		sum += arr[i];
+	}
+	return sum;
 }
 
 function differenceMaxMinWorker(...arr) {
-
+	if (arr.length === 0) {
+		return 0;
+	}
+	let min = Math.min.apply(null, arr);
+	let max = Math.max.apply(null, arr);
+	return max - min;
 }
 
 function differenceEvenOddWorker(...arr) {
-
+	if (arr.length === 0) {
+		return 0;
+	}
+	let sumEvenElement = 0;
+	let sumOddElement = 0;
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] % 2 === 0) {
+			sumEvenElement += arr[i];
+		} else {
+			sumOddElement += arr[i];
+		}
+	}
+	return sumEvenElement - sumOddElement;
 }
 
 function averageEvenElementsWorker(...arr) {
-
+	if (arr.length === 0) {
+		return 0;
+	}
+	let sumEvenElement = 0;
+	let countEvenElement = 0;
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] % 2 === 0) {
+			sumEvenElement += arr[i];
+			countEvenElement++;
+		}
+	}
+	return sumEvenElement / countEvenElement;
 }
 
-function makeWork (arrOfArr, func) {
-
+function makeWork(arrOfarr, func) {
+	let maxWorkerResult = -Infinity;
+	for (let i = 0; i < arrOfarr.length; i++) {
+		const result = func(...arrOfarr[i]);
+		if (result > maxWorkerResult) {
+			maxWorkerResult = result;
+		}
+	}
+	return maxWorkerResult;
 }
