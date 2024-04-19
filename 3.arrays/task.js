34744b895951eaaedef2b05a9027d6802c5f1bf1
NetoIQA75 <all-slavin@yandex.ru>
2024-04-19 15:33:30 +0300

    Third task solved

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,16 @@
 function compareArrays(arr1, arr2) {
-  
+	return (
+		arr1.length === arr2.length &&
+		arr1.every((item, index) => item === arr2[index])
+	);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+	const usersWithGender = users.filter(user => user.gender === gender);
+	if (usersWithGender.length === 0) {
+		return 0;
+	} else {
+		const averageAge = usersWithGender.reduce((sum, user) => sum + user.age, 0) / usersWithGender.length;
+		return averageAge;
+	}
 }
