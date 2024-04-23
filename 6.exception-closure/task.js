9cbba21f8a09741222b80d9e2684b7db87c4d86a
NetoIQA75 <all-slavin@yandex.ru>
2024-04-23 12:45:40 +0300

    Tasks solved

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,50 @@
-﻿
+﻿function parseCount(value) {
+	let parsedValue = Number.parseFloat(value);
+	if (isNaN(parsedValue)) {
+		throw new Error("Невалидное значение");
+	}
+	return parsedValue;
+}
+
+function validateCount(value) {
+	try {
+		return parseCount(value);
+	} catch (error) {
+		return error;
+	}
+}
+
+class Triangle {
+	constructor(a, b, c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+		if (a + b < c || a + c < b || b + c < a) {
+			throw new Error("Треугольник с такими сторонами не существует");
+		}
+	}
+
+	getPerimeter() {
+		return this.a + this.b + this.c;
+	}
+
+	getArea() {
+		const p = this.getPerimeter() / 2; // полупериметр треугольника
+		return Number(Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(3));
+	}
+}
+
+function getTriangle(a, b, c) {
+	try {
+		return new Triangle(a, b, c);
+	} catch (error) {
+		return {
+			getPerimeter() {
+				return "Ошибка! Треугольник не существует";
+			},
+			getArea() {
+				return "Ошибка! Треугольник не существует";
+			}
+		};
+	}
+}
