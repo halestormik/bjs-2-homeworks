2484f86cff59e4026b7d14377fb3894e7fb0de95
NetoIQA75 <all-slavin@yandex.ru>
2024-04-23 16:15:33 +0300

    Don't know why tests don't work

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,48 +1,52 @@
-﻿function parseCount(value) {
+﻿function parseCount(value){
 	let parsedValue = Number.parseFloat(value);
 	if (isNaN(parsedValue)) {
-		throw new Error("Невалидное значение");
+	  throw new Error("Невалидное значение");
 	}
 	return parsedValue;
-}
-
-function validateCount(value) {
+  }
+  
+  function validateCount(value) {
 	try {
-		return parseCount(value);
+	  return parseCount(value);
 	} catch (error) {
-		return error;
+	  return error;
 	}
-}
-
-class Triangle {
+  }
+  
+  class Triangle {
 	constructor(a, b, c) {
-		if ((a + b) < c || (b + c) < a || (a + c) < b) {
-			throw new Error(`Треугольник с такими сторонами не существует`);
-		}
-		this.a = a;
-		this.b = b;
-		this.c = c;
+	  if ( (a + b) < c || (b + c) < a || (a + c) < b) {
+		throw new Error(`Треугольник с такими сторонами не существует`);
+	  }
+	  this.a = a;
+	  this.b = b;
+	  this.c = c;
 	}
 	get perimeter() {
-		return +(this.a + this.b + this.c);
+	  return +(this.a + this.b + this.c);
 	}
 	get area() {
-		let p = this.perimeter / 2;
-		return +(Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c))).toFixed(3);
+	  let p = this.perimeter / 2;
+	  return +( Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)) ).toFixed(3);
 	}
-}
-
-function getTriangle(a, b, c) {
+  }
+  
+  function getTriangle(a, b, c) {
 	try {
-		return new Triangle(a, b, c);
+	  return new Triangle(a, b, c);
 	} catch (error) {
-		return {
-			get perimeter() {
-				return `Ошибка! Треугольник не существует`;
-			},
-			get area() {
-				return `Ошибка! Треугольник не существует`;
-			}
+	  return {
+		get perimeter() {
+		  return "Ошибка! Треугольник не существует";
+		},
+		get area() {
+		  return "Ошибка! Треугольник не существует";
 		}
+	  }
 	}
-}
+  }
+  
+  const triangle = getTriangle(1,3,100);
+  console.log(triangle.perimeter);
+  console.log(triangle.area);
