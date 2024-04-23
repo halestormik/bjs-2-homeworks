2d45d85c4dcd7aba44e53427cceb62138d558e55
NetoIQA75 <all-slavin@yandex.ru>
2024-04-23 16:07:59 +0300

    And again some fixes added

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -16,21 +16,19 @@ function validateCount(value) {
 
 class Triangle {
 	constructor(a, b, c) {
+		if ((a + b) < c || (b + c) < a || (a + c) < b) {
+			throw new Error(`Треугольник с такими сторонами не существует`);
+		}
 		this.a = a;
 		this.b = b;
 		this.c = c;
-		if (a + b < c || a + c < b || b + c < a) {
-			throw new Error("Треугольник с такими сторонами не существует");
-		}
 	}
-
 	get perimeter() {
-		return this.a + this.b + this.c;
+		return +(this.a + this.b + this.c);
 	}
-
 	get area() {
-		const p = this.perimeter / 2; // полупериметр треугольника
-		return Number(Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(3));
+		let p = this.perimeter / 2;
+		return +(Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c))).toFixed(3);
 	}
 }
 
@@ -39,12 +37,12 @@ function getTriangle(a, b, c) {
 		return new Triangle(a, b, c);
 	} catch (error) {
 		return {
-			perimeter() {
-				return "Ошибка! Треугольник не существует";
+			get perimeter() {
+				return `Ошибка! Треугольник не существует`;
 			},
-			area() {
-				return "Ошибка! Треугольник не существует";
+			get area() {
+				return `Ошибка! Треугольник не существует`;
 			}
-		};
+		}
 	}
 }
