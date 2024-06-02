2bee503b62e21f5feae28db834ad7f65376a5a9b
NetoIQA75 <all-slavin@yandex.ru>
2024-06-02 20:16:39 +0300

    Some fixes added again

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -5,14 +5,16 @@ function cachingDecoratorNew(func) {
     const hash = md5(args);
     let objectInCache = cache.find( item => item.hash === hash);
     if (objectInCache) {
-      return `Из кеша: ${objectInCache.value}`;
+      console.log("Из кеша: " + ${objectInCache.value});
+      return "Из кеша: " + ${objectInCache.value};
     }
     let result = func(...args);
     cache.push({"hash": hash, "value": result});
     if (cache.length > 5) {
       cache.shift();
     }
-    return `Вычисляем: ${result}`;
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
   }
   return wrapper;
 }
